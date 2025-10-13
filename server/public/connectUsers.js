deb0dff768978a4f7e858ff08eadfd24883a00da
Subhash Kumar <subhashkushwah134@gmail.com>
2025-10-14 00:19:54 +0530

    api added

--- server/public/connectUsers.js
+++ server/public/connectUsers.js
@@ -1,30 +1,8 @@
-// const checkAttack = async (username, input) => {
-//     const input1 = document.getElementById(input).value;
-
-//     const res = await fetch(
-//         "http://localhost:5000/api/attacks/analyze",
-//         {
-//             method: "POST",
-//             headers: {
-//                 "Content-Type": "application/json",
-//             },
-//             body: JSON.stringify({ url: input1, user: username })
-//         }
-//     );
-
-//     const data = await res.json();
-//     // if(data.attack.status !== "Safe"){
-//     //     alert("Attempted : " +data.attack.attack_type)
-//     // }
-//     console.log(data);
-// }
-
-
 const checkAttack = async (email, ...inputs) => {
     const inputValues = inputs.map(id => document.getElementById(id).value);
 
     try {
-        const res = await fetch("http://localhost:5000/api/attacks/analyze",
+        const res = await fetch(process.env.MY_API,
             {
                 method: "POST",
                 headers: {
